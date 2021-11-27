6c5debb7caeb736386df4fff7c187a40fa06cdd1
Nikolai Amosov <nikolai_sergeevich@mail.ru>
2021-11-27 17:13:10 +0300

    add new user

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { UserDTO } from 'src/app/interfaces/user.interface';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.scss']
 })
-export class AddUserComponent implements OnInit {
+export class AddUserComponent implements OnDestroy {
 
-  constructor() { }
+  private subscriptions = new Subscription();
+  @Input() public userAdd = { name: '', email: '', phone: 0 };
 
-  ngOnInit(): void {
-  }
+  constructor(
+    public userService: UserService, 
+    public router: Router
+  ) { }
 
+  public addUser(data: UserDTO): void {
+    const subscription = this.userService.addUser(this.userAdd)
+    .subscribe((data: {}) => {
+      this.router.navigate(['/list'])
+    })
+    this.subscriptions.add(subscription);
+  };
+ 
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  };
 }
