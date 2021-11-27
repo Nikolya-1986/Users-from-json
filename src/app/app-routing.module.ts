819332cb4d2641c109e327d96429e2c1aa5fb7b7
Nikolai Amosov <nikolai_sergeevich@mail.ru>
2021-11-27 16:14:33 +0300

    routing for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add' },
+  { path: 'add', component: AddUserComponent },
+  { path: 'edit/:id', component: EditUserComponent },
+  { path: 'list', component: UsersListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
